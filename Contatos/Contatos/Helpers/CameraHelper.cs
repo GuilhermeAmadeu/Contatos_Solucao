19fec83311e0590cdc4e876c04d6b4affd4c5525
senac <senac@SJR0561234W10-2>
2017-11-13 17:39:32 -0200

    13

---
+++ Contatos/Contatos/Helpers/CameraHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Media.Abstractions;
+using Plugin.Media;
+
+namespace Contatos.Helpers
+{
+    public static class CameraHelper
+    {
+        public static async Task<MediaFile> TirarFotoAsync(string nomeArquivo)
+        {
+            //Inicializa a camera do dispositivo
+            await CrossMedia.Current.Initialize();
+            //verificar se existe uma camera e esta preparada para tirar fotos
+            if(!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await App.DialogoAlerta("Atenção", "Camera não suportada", "Fechar");
+                return null;
+            }
+
+            //verifica se foi informado um nome para o arquivo
+            if (string.IsNullOrWhiteSpace(nomeArquivo))
+            {
+                nomeArquivo = Guid.NewGuid().ToString();
+                nomeArquivo += ".jpg";
+            }
+            //armazena a foto ao tirar
+            var midia = new StoreCameraMediaOptions();
+            //salva no album de foto do dispositivo
+            midia.SaveToAlbum = true;
+            midia.CompressionQuality = 60;
+            midia.PhotoSize = PhotoSize.Medium;
+            midia.Name = nomeArquivo;
+            //retornar o arquivo
+            var foto = await CrossMedia.Current.TakePhotoAsync(midia);
+            return foto;
+            
+        }
+    }
+}
---
+++ Contatos/Contatos/Pages/CameraPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+
+namespace Contatos.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CameraPage : ContentPage
+	{
+		public CameraPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void tprVoltar_Tapped(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tprCamera_Tapped(object sender, EventArgs e)
+        {
+            //pega a foto tirada
+            arquivoMidia = await Contatos.Helpers.CameraHelper.TirarFotoAsync("foto"); 
+        }
+
+        private void tprAlbum_Tapped(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tprTrocar_Tapped(object sender, EventArgs e)
+        {
+
+        }
+        private void ExibirFoto()
+        {
+            //verifica se existe uma foto para exibicao
+            if(arquivoMidia != null)
+            {
+                //armazena ocaminho onde esta a foto tirada
+                //string caminho = arquivoMidia.Path;
+                //var streamFoto = arquivoMidia.GetStream();
+                //converte a foto armazenada na pasta em formato de memoria
+                imgFoto.Source = ImageSource.FromStream(() =>
+                {
+                    var stream = arquivoMidia.GetStream();
+                    return stream;
+                });
+
+                //imgFoto.Source = ImageSource.FromUri(caminhoFoto);
+            }
+        }
+
+    }
+}
