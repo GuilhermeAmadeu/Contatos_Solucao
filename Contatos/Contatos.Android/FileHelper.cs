cba93326689e497687f966780aa46910b7bfbcc3
senac <senac@SJR0561234W10-2>
2017-11-22 14:18:41 -0200

    felipe preguiçoso

---
+++ Contatos/Contatos.Android/FileHelper.cs
@@ -0,0 +1,22 @@
+﻿using Contatos.Droid;
+using System;
+using System.IO;
+using Xamarin.Forms;
+
+
+
+[assembly: Dependency(typeof(FileHelper))]
+namespace Contatos.Droid
+{
+    public class FileHelper : IFileHelper
+    {
+        public string GetLocalFilePath(string fileName)
+        {
+            //obter a pasta da aplicacao
+            var folder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+
+            //retornar o caminho do arquivo
+            return Path.Combine(folder, fileName);
+        }
+    }
+}
