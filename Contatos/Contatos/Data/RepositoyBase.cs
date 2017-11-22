ffd3286dbb1e7f8ad14a90caedfed117de668a28
senac <senac@SJR0561234W10-2>
2017-11-22 15:44:53 -0200

    sqlite installed

---
+++ Contatos/Contatos/Data/RepositoyBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Contatos.Data
+{
+    public class RepositoyBase
+    {
+        public RepositoyBase()
+        {
+
+        }
+        private void Inicializar()
+        {
+            //obter a implementaçao da interface de acordo com a app
+            IFileHelper fh = DependencyService.Get<IFileHelper>();
+
+            //caminho do arquivo do banco
+            var caminho = fh.GetLocalFilePath("ContatosSQLite.db3");
+
+            //instanciar a conexao
+        }
+    }
+}
