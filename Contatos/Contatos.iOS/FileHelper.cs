6b9428c2ed55df76f8a73de9698abac29cb0bda9
senac <senac@SJR0561234W10-2>
2017-11-22 14:53:23 -0200

    meio da aula 15:00

---
+++ Contatos/Contatos.iOS/FileHelper.cs
@@ -0,0 +1,31 @@
+﻿using Contatos.iOS;
+using System;
+using System.IO;
+using Xamarin.Forms;
+
+
+namespace Contatos.iOS
+{
+    public class FileHelper : IFileHelper
+    {
+        public string GetLocalFilePath(string fileName)
+        {
+            //OBTER A PASTA PESSOAL
+            var personalfolder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+
+            //CONCATENAR A PASTA LIBRARY
+            var folder = Path.Combine(personalfolder, "..", "Library");
+
+            //VERIFICAR SE A PASTA EXISTE
+            if (!Directory.Exists(folder))
+            {
+                //CRIAR A PASTA 
+                Directory.CreateDirectory(folder);
+
+            }
+
+            //RETORNAR O CAMINHO DO ARQUIVO
+            return Path.Combine(folder, fileName);
+        }
+    }
+}
