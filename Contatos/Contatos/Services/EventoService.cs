b62adf1ab1c644f81292fa00cf0de5ef304bc8da
Gui <gui_amadeu3@hotmail.com>
2017-12-04 17:22:48 -0200

    adicionado a parte de add/excluir do eventos

--- Contatos/Contatos/Services/EventoService.cs
+++ Contatos/Contatos/Services/EventoService.cs
@@ -9,141 +9,139 @@ using Contatos.Models;
 
 namespace Contatos.Services
 {
-        public class EventoService : ServiceBase
+    public class EventoService : ServiceBase
+    {
+        public EventoService() : base()
         {
-            public EventoService() : base()
+            //definir o endereço do cliente 
+            cliente.BaseAddress = new Uri(string.Format(Constantes.URL_SERVICO, "eventoapi"));
+        }
+        public async Task<List<Evento>> PesquisarAsync(string conteudo)
+        {
+            //itens que serão retornados pela pesquisa 
+            var itens = new List<Evento>();
+
+            //Instanciar o resultado da operacao 
+            Resultado = new ResultadoOperacao()
             {
-                //definir o endereço do cliente 
-                cliente.BaseAddress = new Uri(string.Format(Constantes.URL_SERVICO, "pessoaapi"));
-            }
-            public async Task<List<Evento>> PesquisarAsync(string conteudo)
+                Sucesso = false
+            };
+
+            //Criar o parametro para chamada
+            var parametro = "?content=" + conteudo;
+
+            try
             {
-                //itens que serão retornados pela pesquisa 
-                var itens = new List<Evento>();
+                //chamar o servico
+                var resposta = await cliente.GetAsync(parametro);
 
-                //Instanciar o resultado da operacao 
-                Resultado = new ResultadoOperacao()
+                //verificar se executou com sucesso
+                if (resposta.IsSuccessStatusCode)
                 {
-                    Sucesso = false
-                };
+                    //obter o conteudo da resposta
+                    var conteudoret = await resposta.Content.ReadAsStringAsync();
 
-                //Criar o parametro para chamada
-                var parametro = "?content=" + conteudo;
+                    //transformar o conteudo retornado na lista de itens
+                    itens = JsonConvert.DeserializeObject<List<Evento>>(conteudoret);
 
-                try
-                {
-                    //chamar o servico
-                    var resposta = await cliente.GetAsync(parametro);
-
-                    //verificar se executou com sucesso
-                    if (resposta.IsSuccessStatusCode)
-                    {
-                        //obter o conteudo da resposta
-                        var conteudoret = await resposta.Content.ReadAsStringAsync();
-
-                        //transformar o conteudo retornado na lista de itens
-                        itens = JsonConvert.DeserializeObject<List<Pessoa>>(conteudoret);
-
-                        //assinalar o resultado com sucesso
-                        Resultado.Sucesso = true;
-                    }
-                    else
-                    {
-                        //obter a mensagem de rro
-                        Resultado.Mensagem = await resposta.Content.ReadAsStringAsync();
-                        Resultado.NumeroErro = (int)resposta.StatusCode;
-                    }
+                    //assinalar o resultado com sucesso
+                    Resultado.Sucesso = true;
                 }
-                catch (Exception ex)
+                else
                 {
-                    //obter a mensagem de erro 
-                    Resultado.Mensagem = ex.Message;
-                }
-                return itens;
-            }
-            public async Task SalvarAsync(Pessoa item, bool ehNovo = false)
-            {
-                //Inicializar o resultado da operaçao
-                Resultado = new ResultadoOperacao()
-                {
-                    Sucesso = false
-                };
-
-                try
-                {
-                    //converter os dados para json
-                    var json = JsonConvert.SerializeObject(item);
-
-                    //criar o conteudo para o envio dos dados json
-                    var conteudo = new StringContent(json, Encoding.UTF8, Constantes.CONTEUDO_JSON);
-
-                    //declarar o objeto de resposta http da requisicao
-                    HttpResponseMessage resposta = null;
-
-                    //verificar se é um item para inclusão
-                    if (ehNovo == true)
-                    {
-                        resposta = await cliente.PostAsync("", conteudo);
-                    }
-                    else
-                    {
-                        resposta = await cliente.PutAsync("", conteudo);
-                    }
-
-                    //verificar se houve sucesso na chamada
-                    if (resposta.IsSuccessStatusCode)
-                    {
-                        Resultado.Sucesso = true;
-                    }
-                    else
-                    {
-                        //obter as informaçoes da requisiçao
-                        Resultado.Mensagem = await resposta.Content.ReadAsStringAsync();
-                        Resultado.NumeroErro = (int)resposta.StatusCode;
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Resultado.Mensagem = ex.Message;
+                    //obter a mensagem de rro
+                    Resultado.Mensagem = await resposta.Content.ReadAsStringAsync();
+                    Resultado.NumeroErro = (int)resposta.StatusCode;
                 }
             }
-
-            //deletar item
-            public async Task ExcluirAsync(Pessoa item)
+            catch (Exception ex)
             {
-                //Inicializar o resultado da operaçao
-                Resultado = new ResultadoOperacao()
+                //obter a mensagem de erro 
+                Resultado.Mensagem = ex.Message;
+            }
+            return itens;
+        }
+        public async Task SalvarAsync(Evento item, bool ehNovo = false)
+        {
+            //Inicializar o resultado da operaçao
+            Resultado = new ResultadoOperacao()
+            {
+                Sucesso = false
+            };
+
+            try
+            {
+                //converter os dados para json
+                var json = JsonConvert.SerializeObject(item);
+
+                //criar o conteudo para o envio dos dados json
+                var conteudo = new StringContent(json, Encoding.UTF8, Constantes.CONTEUDO_JSON);
+
+                //declarar o objeto de resposta http da requisicao
+                HttpResponseMessage resposta = null;
+
+                //verificar se é um item para inclusão
+                if (ehNovo == true)
                 {
-                    Sucesso = false
-                };
-
-                //criar o parametro para requisicao 
-                var parametro = "?id=" + item.Id.ToString();
-
-                try
-                {
-
-                    var resposta = await cliente.DeleteAsync(parametro);
-
-                    //verificar se houve sucesso na chamada
-                    if (resposta.IsSuccessStatusCode)
-                    {
-                        Resultado.Sucesso = true;
-                    }
-                    else
-                    {
-                        //obter as informaçoes da requisiçao
-                        Resultado.Mensagem = await resposta.Content.ReadAsStringAsync();
-                        Resultado.NumeroErro = (int)resposta.StatusCode;
-                    }
+                    resposta = await cliente.PostAsync("", conteudo);
                 }
-                catch (Exception ex)
+                else
                 {
-                    Resultado.Mensagem = ex.Message;
+                    resposta = await cliente.PutAsync("", conteudo);
                 }
 
+                //verificar se houve sucesso na chamada
+                if (resposta.IsSuccessStatusCode)
+                {
+                    Resultado.Sucesso = true;
+                }
+                else
+                {
+                    //obter as informaçoes da requisiçao
+                    Resultado.Mensagem = await resposta.Content.ReadAsStringAsync();
+                    Resultado.NumeroErro = (int)resposta.StatusCode;
+                }
+            }
+            catch (Exception ex)
+            {
+                Resultado.Mensagem = ex.Message;
             }
         }
 
+        //deletar item
+        public async Task ExcluirAsync(Evento item)
+        {
+            //Inicializar o resultado da operaçao
+            Resultado = new ResultadoOperacao()
+            {
+                Sucesso = false
+            };
+
+            //criar o parametro para requisicao 
+            var parametro = "?id=" + item.Id.ToString();
+
+            try
+            {
+
+                var resposta = await cliente.DeleteAsync(parametro);
+
+                //verificar se houve sucesso na chamada
+                if (resposta.IsSuccessStatusCode)
+                {
+                    Resultado.Sucesso = true;
+                }
+                else
+                {
+                    //obter as informaçoes da requisiçao
+                    Resultado.Mensagem = await resposta.Content.ReadAsStringAsync();
+                    Resultado.NumeroErro = (int)resposta.StatusCode;
+                }
+            }
+            catch (Exception ex)
+            {
+                Resultado.Mensagem = ex.Message;
+            }
+
+        }
     }
 }
